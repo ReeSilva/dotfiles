f2d954a4f8586ebfd064857fa77743185012606a
Renato Biancalana da Silva <renato.silva@quandoo.com>
2023-02-18 04:56:44 -0300

    clean fish to fisher handle plugins

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+for file in $__fish_config_dir/custom.d/*.fish
+  source $file
+end
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/opt/google-cloud-sdk/path.fish.inc' ]; . '/opt/google-cloud-sdk/path.fish.inc'; end
 
