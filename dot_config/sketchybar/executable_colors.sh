0eb7f30fbef5cea02d366160bdc83aa7a558656f
Renato Biancalana da Silva <renato.biancalana@taxfix.de>
2024-12-08 22:44:17 +0100

    updated for nix-darwin

--- dot_config/sketchybar/executable_colors.sh
+++ dot_config/sketchybar/executable_colors.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
 
 # Color Palette
-export BLACK=0xff2e3440
-export WHITE=0xffd8dee9
-export RED=0xffbf616a
-export GREEN=0xffa3be8c
-export BLUE=0xff5e81ac
-export YELLOW=0xffebcb8b
-export ORANGE=0xffd08770
-export MAGENTA=0xffb48ead
-export GREY=0xff4c566a
+export BLACK=0xff4c4f69
+export WHITE=0xffeff1f5
+export RED=0xffd20f39
+export GREEN=0xff40a02b
+export BLUE=0xff1e66f5
+export YELLOW=0xffdf8e1d
+export ORANGE=0xfffe640b
+export MAGENTA=0xffea76cb
+export GREY=0xff9ca0b0
 export TRANSPARENT=0xff000000
-export BLUE2=0xff81a1c1
+export BLUE2=0xff7287fd
 
 # General bar colors
-export BAR_COLOR=0xff2e3440 # Color of the bar
-export ICON_COLOR=$WHITE # Color of all icons
-export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0xff3b4252
-export BACKGROUND_2=0xff434c5e
+export BAR_COLOR=0xeff1f5ff # Color of the bar
+export ICON_COLOR=0xff4c4f69
+export LABEL_COLOR=0xff4c4f69 # Color of all labels
+export BACKGROUND_1=0xffbcc0cc
+export BACKGROUND_2=0xffbcc0cc
 
 export POPUP_BACKGROUND_COLOR=$BLACK
 export POPUP_BORDER_COLOR=$WHITE
